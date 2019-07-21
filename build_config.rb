2c4ae1b9cbbb451df8f43c2f22759234b1ae7a8b
takumin <takumiiinn@gmail.com>
2019-07-21 10:41:52 +0900

    cleanup build_config

--- build_config.rb
+++ build_config.rb
@@ -1,10 +1,6 @@
-def gem_config(conf)
-  conf.gem File.expand_path(File.dirname(__FILE__))
-end
-
 MRuby::Build.new do |conf|
   toolchain :gcc
-  gem_config(conf)
   enable_debug
   conf.enable_test
+  conf.gem File.expand_path(File.dirname(__FILE__))
 end
